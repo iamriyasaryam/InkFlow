2c9094c2f5e765bf725cb2995d22bd4225cfe59d
Riya <iamriyasaryam@gmail.com>
2026-09-25 23:06:50 +0530

    Add: Create Post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import models 
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    posts = models.Post.objects.all().order_by('-created_at')
+    return render(request, 'index.html', {'posts': posts})
+
+def create_post(request):
+
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        content = request.POST.get('content')
+
+        models.Post.objects.create(
+            title=title,
+            author=author,
+            content=content
+        )
+
+        return redirect('home')
+
+    return render(request, 'create_post.html')
